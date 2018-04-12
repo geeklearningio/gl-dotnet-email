bf3828eaaaa0cd5b0f45fbb8692b0badc243b576
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2018-04-12 14:32:43 +1000

    stupid me

--- src/GeekLearning.Email.SendGrid/SendGridEmailHelpers.cs
+++ src/GeekLearning.Email.SendGrid/SendGridEmailHelpers.cs
@@ -1,13 +1,13 @@
-﻿using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GeekLearning.Email.SendGrid
+﻿namespace GeekLearning.Email.SendGrid
 {
+    using global::SendGrid.Helpers.Mail;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     internal static class SendGridEmailHelpers
     {
-        internal EmailAddress ToSendGridEmail(this IEmailAddress email)
+        internal static EmailAddress ToSendGridEmail(this IEmailAddress email)
         {
             return new EmailAddress(email.Email, email.DisplayName);
         }
