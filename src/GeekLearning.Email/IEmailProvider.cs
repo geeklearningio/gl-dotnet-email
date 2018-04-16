38b5216e0f0ad72be4c8d23dc0c7a76b54b19234
Budsy <dickow@turbonet.com>
2018-04-16 14:49:19 -0700

    Update IEmailProvider.cs

--- src/GeekLearning.Email/IEmailProvider.cs
+++ src/GeekLearning.Email/IEmailProvider.cs
@@ -1,11 +1,18 @@
-﻿namespace GeekLearning.Email
+namespace GeekLearning.Email
 {
+
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using MimeKit;
 
     public interface IEmailProvider
     {
-        Task SendEmailAsync(IEmailAddress from, IEnumerable<IEmailAddress> recipients, string subject, string bodyText, string bodyHtml);
+        Task SendEmailAsync(
+            IEmailAddress from, 
+            IEnumerable<IEmailAddress> recipients, 
+            string subject, 
+            string bodyText, 
+            string bodyHtml, 
+            AttachmentCollection attachments=null);
     }
 }
-
