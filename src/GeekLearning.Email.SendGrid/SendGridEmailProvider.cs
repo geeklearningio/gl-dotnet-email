4e044ddd207033828674ac25efc6f1bcaeffc0a5
Budsy <dickow@turbonet.com>
2018-04-16 15:01:16 -0700

    Update SendGridEmailProvider.cs

--- src/GeekLearning.Email.SendGrid/SendGridEmailProvider.cs
+++ src/GeekLearning.Email.SendGrid/SendGridEmailProvider.cs
@@ -1,7 +1,8 @@
-﻿namespace GeekLearning.Email.SendGrid
+namespace GeekLearning.Email.SendGrid
 {
     using global::SendGrid;
     using global::SendGrid.Helpers.Mail;
+    using MimeKit;
     using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
@@ -23,21 +24,17 @@
             }
         }
 
-        public async Task SendEmailAsync(
-            IEmailAddress from,
-            IEnumerable<IEmailAddress> recipients,
-            string subject,
-            string text,
-            string html)
-        {
+  
 
+        public async Task SendEmailAsync(IEmailAddress from, IEnumerable<IEmailAddress> recipients, string subject, string bodyText, string bodyHtml, AttachmentCollection attachments = null)
+        {
             var client = new SendGridClient(this.apiKey);
 
             SendGridMessage message;
 
             if (recipients.Count() == 1)
             {
-                message = MailHelper.CreateSingleEmail(from.ToSendGridEmail(), recipients.First().ToSendGridEmail(), subject, text, html);
+                message = MailHelper.CreateSingleEmail(from.ToSendGridEmail(), recipients.First().ToSendGridEmail(), subject, bodyText, bodyHtml);
             }
             else
             {
@@ -45,8 +42,8 @@
                     from.ToSendGridEmail(),
                     recipients.Select(email => email.ToSendGridEmail()).ToList(),
                     subject,
-                    text,
-                    html);
+                    bodyText,
+                    bodyHtml);
             }
 
             var response = await client.SendEmailAsync(message);
@@ -57,3 +54,4 @@
         }
     }
 }
+
