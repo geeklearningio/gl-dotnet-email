2678c86feedb4b12654f0531157ed61fc7ea857c
Budsy <dickow@turbonet.com>
2018-04-16 14:50:07 -0700

    Update IEmailAddress.cs

--- src/GeekLearning.Email/IEmailAddress.cs
+++ src/GeekLearning.Email/IEmailAddress.cs
@@ -1,9 +1,14 @@
-﻿namespace GeekLearning.Email
+namespace GeekLearning.Email
 {
+    public enum AddressTarget {  From, To, Cc, Bcc, ReplyTo}
+
     public interface IEmailAddress
     {
         string Email { get; }
 
         string DisplayName { get; }
+
+        AddressTarget AddressAs { get; }
+  
     }
 }
