1a26712864b17ecf26ebff416912b4f24e115ab0
Budsy <dickow@turbonet.com>
2018-04-16 14:04:32 -0700

    Delete IntegrationCollection.cs

--- IntegrationCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeekLearning.Email.Integration.Test
-{
-    using Xunit;
-
-    [CollectionDefinition(nameof(IntegrationCollection))]
-    public class IntegrationCollection: ICollectionFixture<StoresFixture>
-    {
-    }
-}
