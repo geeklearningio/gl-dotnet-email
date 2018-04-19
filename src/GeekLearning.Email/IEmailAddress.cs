3d2e9897250273bbf5ca25795b3ff62dad8a6c4a
Budsy <dickow@turbonet.com>
2018-04-19 13:49:50 -0700

    Update IEmailAddress.cs
    Interface for IEmailAddress restored to original, simple form.

--- src/GeekLearning.Email/IEmailAddress.cs
+++ src/GeekLearning.Email/IEmailAddress.cs
@@ -1,13 +1,9 @@
 namespace GeekLearning.Email
 {
-    public enum AddressTarget {  From, To, Cc, Bcc, ReplyTo}
-    
     public interface IEmailAddress
     {
         string Email { get; }
 
         string DisplayName { get; }
-        
-        AddressTarget AddressAs { get;  } 
     }
 }
