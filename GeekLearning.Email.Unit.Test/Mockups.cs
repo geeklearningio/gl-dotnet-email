8435a685248d756baafc35aec53a3bfbd3ca6e39
Anna Yafi <annayafi@gmail.com>
2018-10-02 21:49:24 +0800

    test important functions for cc

--- GeekLearning.Email.Unit.Test/Mockups.cs
+++ GeekLearning.Email.Unit.Test/Mockups.cs
@@ -31,6 +31,27 @@ namespace GeekLearning.Email.Unit.Test
                 Enumerable.Empty<IEmailAttachment>()), Times.Once);
 
         }
+
+        [Fact]
+        public void TestMockupRecipientsCc()
+        {
+            var emailProvider = new Mock<IEmailProvider>();
+            var emailProviderTypes = new List<IEmailProviderType> { new TestProviderType(emailProvider.Object) };
+
+            var emailSender = new EmailSender(emailProviderTypes, Datas.GetMockupOptions(), new Mock<IStorageFactory>().Object, new Mock<ITemplateLoaderFactory>().Object);
+            emailSender.SendEmailAsync(Datas.DefaultSender, "test", "test", Enumerable.Empty<IEmailAttachment>(), new EmailAddress("test@test.fr", "test").Yield(), new EmailAddress("test@test.fr", "test").Yield(), new EmailAddress[0]);
+
+            emailProvider.Verify(e => e.SendEmailAsync(
+                Datas.DefaultSender,
+                It.Is<IEnumerable<IEmailAddress>>(list => list.All(a => a.DisplayName == "Mockup Recipient" && a.Email == Datas.MockedUpRecipient)),
+                 It.Is<IEnumerable<IEmailAddress>>(list => list.All(a => a.DisplayName == "Mockup Recipient" && a.Email == Datas.MockedUpRecipient)),
+                It.IsAny<IEnumerable<IEmailAddress>>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                Enumerable.Empty<IEmailAttachment>()), Times.Once);
+
+        }
     }
 
     public class TestProviderType : IEmailProviderType
